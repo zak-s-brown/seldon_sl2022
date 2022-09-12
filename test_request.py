4c5413c63789ff2afd115a3e4523dc8bd25b9ee6
zak-s-brown <zach.brown@baltosoftware.com>
2022-09-12 10:28:37 -0400

    add rpunct and sentiment

--- test_request.py
+++ test_request.py
@@ -18,3 +18,19 @@ response = requests.post(
     },
 )
 print(response.text)
+
+response = requests.post(
+    "http://localhost:8080/seldon/default/sentiment/api/v1.0/predictions",
+    headers={"Content-Type": "application/json"},
+    json={
+        "data": {
+            "ndarray": 
+                [
+
+                        "I really hate how cloudy it is today"   
+                ]
+            
+        }
+    },
+)
+print(response.text)
