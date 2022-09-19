564e2c10637f7146eb4597004c59eb81f5a7a885
zak-s-brown <zach.brown@baltosoftware.com>
2022-09-19 16:44:29 -0500

    update service build

--- build_service.sh
+++ build_service.sh
@@ -8,5 +8,13 @@ docker build -t $svc:latest .
 echo "Tagging and Deploying"
 cd ../../
 docker tag $svc:latest localhost:5001/$svc:latest && \
-docker push localhost:5001/$svc:latest && \
-kubectl apply -f k8s/$svc-deploy.yml
+docker push localhost:5001/$svc:latest 
+
+# Combiner doesn't have a deployment
+if [[ -f "k8s/$svc-deploy.yml" ]]; then
+    echo "found deployment: k8s/$svc-deploy.yml"
+    echo "deploying"
+    kubectl apply -f k8s/$svc-deploy.yml
+fi
+
+
